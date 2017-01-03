d874fa806dce824ce61be60b52b2eb37d84a27f1
minh duong <mcd4874@g.rit.edu>
2017-01-03 14:28:24 -0500

    Initial Commit

---
+++ DreamListProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamListProject
+//
+//  Created by minh duong on 1/3/17.
+//  Copyright © 2017 minh duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
